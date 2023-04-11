300e099ce2c9101cb6a62ed369e0c3350f059fad
justJayesh <115465646+justtJayesh@users.noreply.github.com>
2023-04-11 09:13:28 +0530

    contact and about section id's changed

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,47 +4,53 @@ import profilePicture from "../Assets/picture2.jpg";
 
 export const About = () => {
     return (
-        <Box backgroundColor={"#EDF7FA"} display="flex">
-            <Box w={"72%"} margin={"70px auto"}>
-                <Stack
-                    direction={{ lg: "row", md: "column", base: "column" }}
-                    justifyContent={"space-between"}
-                >
-                    <Box w={{ lg: "60%", base: "100%" }}>
-                        <Box
-                            padding={"0 20px"}
-                            color={"gray.900"}
-                            borderLeft={"3px solid gray"}
-                        >
-                            <Heading>About</Heading>
-                        </Box>
-                        <Box h={"7"}></Box>
-                        <Text
-                            textAlign={"justify"}
-                            color={"gray.700"}
-                            fontSize={"lg"}
-                            id="user-detail-intro"
-                        >
-                            I'm a Full Stack Web Developer who is passionate
-                            about developing things aesthetically pleasing and
-                            creative to elevate people's interaction with the
-                            product. Some technologies I enjoy working with
-                            include ReactJS, Jamstack (Javascript, API +
-                            Markup), and creating User interfaces using Figma
-                            and Sketch. My Interest in designing led me to
-                            become a Full Stack Web Developer, So that I could
-                            build whatever I want.
-                        </Text>
-                    </Box>
-                    <Box
-                        w={{ lg: "25%", base: "70%" }}
-                        // borderRadius={"70%"}
-                        // border={"1px solid black"}
+        <div id="user-detail-intro">
+            <Box backgroundColor={"#EDF7FA"} display="flex">
+                <Box w={"72%"} margin={"70px auto"}>
+                    <Stack
+                        direction={{ lg: "row", md: "column", base: "column" }}
+                        justifyContent={"space-between"}
                     >
-                        <Image className="home-img" src={profilePicture} borderRadius={"2%"} />
-                    </Box>
-                </Stack>
+                        <Box w={{ lg: "60%", base: "100%" }}>
+                            <Box
+                                padding={"0 20px"}
+                                color={"gray.900"}
+                                borderLeft={"3px solid gray"}
+                            >
+                                <Heading>About</Heading>
+                            </Box>
+                            <Box h={"7"}></Box>
+                            <Text
+                                textAlign={"justify"}
+                                color={"gray.700"}
+                                fontSize={"lg"}
+                            >
+                                I'm a Full Stack Web Developer who is passionate
+                                about developing things aesthetically pleasing
+                                and creative to elevate people's interaction
+                                with the product. Some technologies I enjoy
+                                working with include ReactJS, Jamstack
+                                (Javascript, API + Markup), and creating User
+                                interfaces using Figma and Sketch. My Interest
+                                in designing led me to become a Full Stack Web
+                                Developer, So that I could build whatever I
+                                want.
+                            </Text>
+                        </Box>
+                        <Box
+                            w={{ lg: "25%", base: "70%" }}
+                            // borderRadius={"70%"}
+                            // border={"1px solid black"}
+                        >
+                            <Image
+                                className="home-img"
+                                src={profilePicture}
+                                borderRadius={"2%"}
+                            />
+                        </Box>
+                    </Stack>
+                </Box>
             </Box>
-        </Box>
+        </div>
     );
 };
