d5932efc2502a5e4029bb1aaa7a81d3ef46cf5a9
justJayesh <115465646+justtJayesh@users.noreply.github.com>
2023-05-09 16:05:05 +0530

    fixing navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -54,7 +54,7 @@ export default function Navbar() {
             style={{
                 position: "sticky",
                 top: 0,
-                zIndex: "overlay",
+                zIndex: "1",
                 backgroundColor: "white",
                 // boxShadow: rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px
             }}
